5915f40eff90ce3eacd7366fcb0f6102353ea98b
SecretDevStar <ztoc0130@gmail.com>
2023-02-13 16:15:47 +0900

    finish

--- website/repository_postgresql_classic.go
+++ website/repository_postgresql_classic.go
@@ -3,6 +3,8 @@ package website
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"github.com/jackc/pgconn"
 )
 
 type PostgreSQLClassicRepository struct {
@@ -20,11 +22,41 @@ func (r *PostgreSQLClassicRepository) Migrate(ctx context.Context) error {
 	query := `
 		CREATE TABLE IF NOT EXISTS websites (
 			id SERIAL PRIMARY KEY,
-			name TEX NOT NULL UNIQUE,
+			name TEXT NOT NULL UNIQUE,
 			url 	TEXT NOT NULL,
 			rank 	INT NOT NULL
 		);
 	`
 	_, err := r.db.ExecContext(ctx, query)
 	return err
+}
+
+func (r *PostgreSQLClassicRepository) Create(ctx context.Context, website Website) (*Website, error) {
+	var id int64
+	err := r.db.QueryRowContext(ctx, "INSERT INTO websites(name, url, rank) values($1, $2, $3) RETURNING id", website.Name, website.URL, website.Rank).Scan(&id)
+	if err != nil {
+		var pgxError *pgconn.PgError
+		if errors.As(err, &pgxError) {
+			if pgxError.Code == "23505" {
+				return nil, ErrDuplicate
+			}
+		}
+		return nil, err
+	}
+	website.ID = id
+	return &website, nil
+
+}
+
+func (r *PostgreSQLClassicRepository) All(ctx context.Context) ([]Website, error) {
+	rows, err := r.db.QueryContext(ctx, "SELECT * FROM websites")
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var all []Website
+	for rows.Next() {
+		var website Wet
+	}
 }
